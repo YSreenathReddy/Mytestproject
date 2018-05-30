d696acf993ca563ffcd4b06d3a4101678b1fc81d
Sreenath <Sreenath@Sreenath-PC>
2018-05-30 22:32:26 +0530

    THis is done by sreenath

---
+++ Java/Arrays/Arrays/ArrayElementAssignments.java
@@ -0,0 +1,38 @@
+package Arrays;
+
+public class ArrayElementAssignments {
+
+	public static void main(String[] args)
+	{
+	//Case 2
+		
+		Object[] o= new Object[10];
+		o[0]=new String("sree");
+		o[1]=new StringBuffer("reddy");
+		o[2]=new Integer(10);
+		
+		for(int i=0;i<o.length;i++)
+		System.out.println(o[i]);
+		
+		
+	//Case 3
+		
+		Runnable[] r=new Runnable[10];
+		r[0]=new Thread();
+		
+		System.out.println("hiii");
+		
+		
+		
+				
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ Java/Arrays/Arrays/Arraydeclaration.java
@@ -0,0 +1,38 @@
+package Arrays;
+
+public class Arraydeclaration {
+
+	public static void main(String[] args)
+	{
+		//One dimensional array
+		int[] x=new int[3];
+		x[0]=10;
+		x[1]=20;
+		x[2]=30;
+		System.out.println(x[0]+x[1]+x[2]);
+		
+		//Two dimensional array
+		
+		int[][] x1=new int[4][];
+		x1[0]=new int[2];
+		x1[1]=new int[2];
+		
+		//Multi dimnsion array
+		
+		int[][][] x2=new int[2][][];
+		x2[0]=new int[3][];
+		x2[0][0]=new int[1];
+		x2[0][1]=new int[2];
+		x2[0][2]=new int[3];
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
